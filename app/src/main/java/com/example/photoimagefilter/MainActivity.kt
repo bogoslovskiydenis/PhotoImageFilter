16f5fa02e8e9250a337879b97a9beb63461636ca
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-06-02 12:14:30 +0300

    setListeners buttonEditNewImage setListeners displayImagePreview

--- app/src/main/java/com/example/photoimagefilter/MainActivity.kt
+++ app/src/main/java/com/example/photoimagefilter/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.photoimagefilter
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import com.example.photoimagefilter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val REQUEST_CODE_PICK_IMAGE = 1
+        const val KEY_IMAGE_URL="imageUri"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    setListeners()
+    }
+
+
+    private  fun  setListeners(){
+        binding.buttonEditNewImage.setOnClickListener{
+            Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI).also {
+                    pickerIntent -> pickerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivityForResult(pickerIntent, REQUEST_CODE_PICK_IMAGE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, result: Int, data: Intent?){
+        super.onActivityResult(requestCode, result , data)
+        if(requestCode == REQUEST_CODE_PICK_IMAGE && result == RESULT_OK){
+            data?.data?.let{imageUri->
+                Intent(applicationContext, EditImageActivity:: class.java).also {
+                    editImageIntent->
+                    editImageIntent.putExtra(KEY_IMAGE_URL, imageUri)
+                    startActivity(editImageIntent)
+                }
+            }
+        }
     }
 }
