441753f7040f2426e8e6ff23a22d04f1492bc700
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-06-07 13:52:01 +0300

    observe -> viewModel.saveFilteredImageUiState activity_filtered_image.xml -> shared image

--- app/src/main/java/com/example/photoimagefilter/activities/EditImageActivity.kt
+++ app/src/main/java/com/example/photoimagefilter/activities/EditImageActivity.kt
@@ -1,9 +1,11 @@
 package com.example.photoimagefilter.activities
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
@@ -16,8 +18,13 @@ import com.example.photoimagefilter.utilities.show
 import com.example.photoimagefilter.viewmodel.EditImageViewModel
 import jp.co.cyberagent.android.gpuimage.GPUImage
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.log
 
 class EditImageActivity : AppCompatActivity(), ImageFilterListeners {
+
+    companion object {
+        const val KEY_FILTERED_IMAGE_URI = "filteredImageUrl"
+    }
 
     private lateinit var binding: ActivityEditImageBinding
 
@@ -39,7 +46,7 @@ class EditImageActivity : AppCompatActivity(), ImageFilterListeners {
         prepareImagePreview()
     }
 
-
+    //region : Observers
     private fun setupObservers() {
         //imagePreviewUiState
         viewModel.imagePreviewUiState.observe(this, {
@@ -81,7 +88,34 @@ class EditImageActivity : AppCompatActivity(), ImageFilterListeners {
         filteredBitmap.observe(this, { bitmap ->
             binding.imagePreview.setImageBitmap(bitmap)
         })
+
+        viewModel.saveFilteredImageUiState.observe(this, {
+            val saveFilteredImageDataState = it ?: return@observe
+            if (saveFilteredImageDataState.isLoading) {
+                binding.imageSave.visibility = View.GONE
+                binding.savingProgressBar.visibility = View.VISIBLE
+            } else {
+                binding.savingProgressBar.visibility = View.VISIBLE
+                binding.imageSave.visibility = View.GONE
+            }
+            saveFilteredImageDataState.uri?.let { savedImageUri ->
+                Intent(
+                    applicationContext,
+                    FilteredImageActivity::class.java
+                ).also { filteredImageIntent ->
+                    filteredImageIntent.putExtra(KEY_FILTERED_IMAGE_URI, savedImageUri)
+                    startActivity(filteredImageIntent)
+
+                }
+            } ?: kotlin.run {
+                saveFilteredImageDataState.error?.let { error ->
+                    displayToast(error)
+                }
+            }
+        })
     }
+
+    //endregion
 
     private fun prepareImagePreview() {
 
@@ -106,9 +140,16 @@ class EditImageActivity : AppCompatActivity(), ImageFilterListeners {
         binding.imageback.setOnClickListener {
             onBackPressed()
         }
+        binding.imageSave.setOnClickListener {
+            filteredBitmap.value?.let { bitmap ->
+                viewModel.saveFilteredImage(
+                    bitmap
+                )
+            }
+        }
+
         /*
         This will show image when we long click the ImageView untill we release click
-
          */
         binding.imagePreview.setOnLongClickListener {
             binding.imagePreview.setImageBitmap(originalBitmap)
---
+++ app/src/main/java/com/example/photoimagefilter/activities/FilteredImageActivity.kt
@@ -0,0 +1,41 @@
+package com.example.photoimagefilter.activities
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.photoimagefilter.R
+import com.example.photoimagefilter.databinding.ActivityFilteredImageBinding
+
+class FilteredImageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFilteredImageBinding
+    private lateinit var fileUri: Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFilteredImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        displayFilteredImage()
+        setListeners()
+    }
+
+
+    private fun displayFilteredImage() {
+        intent.getParcelableExtra<Uri>(EditImageActivity.KEY_FILTERED_IMAGE_URI)?.let { imageUri ->
+            fileUri = imageUri
+            binding.imageFilteredImage.setImageURI(imageUri)
+        }
+    }
+
+    private fun setListeners() {
+        binding.btnShare.setOnClickListener {
+            with(Intent(Intent.ACTION_SEND)) {
+                putExtra(Intent.EXTRA_STREAM, fileUri)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                type = "image/*"
+                startActivity(this)
+            }
+        }
+    }
+}
