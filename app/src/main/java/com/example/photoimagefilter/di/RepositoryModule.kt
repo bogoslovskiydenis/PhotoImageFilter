6c355e7e7d4ec94b8428676e49b5430a0197e192
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-06-08 19:20:54 +0300

    viewmodel SavedImagesViewModel dipendencyinjection RepositoryModule & ViewModelModule

--- app/src/main/java/com/example/photoimagefilter/di/RepositoryModule.kt
+++ app/src/main/java/com/example/photoimagefilter/di/RepositoryModule.kt
@@ -2,9 +2,13 @@ package com.example.photoimagefilter.di
 
 import com.example.photoimagefilter.repository.EdiImageRepository
 import com.example.photoimagefilter.repository.EditImageRepositoryImpl
+import com.example.photoimagefilter.repository.SaveImagesRepository
+import com.example.photoimagefilter.repository.SavedImagesRepositoryImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val repositoryModule = module {
     factory<EdiImageRepository> {  EditImageRepositoryImpl(androidContext()) }
+
+    factory<SaveImagesRepository> { SavedImagesRepositoryImpl(androidContext()) }
 }
--- app/src/main/java/com/example/photoimagefilter/di/ViewModelModule.kt
+++ app/src/main/java/com/example/photoimagefilter/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.example.photoimagefilter.di
 
 import com.example.photoimagefilter.viewmodel.EditImageViewModel
+import com.example.photoimagefilter.viewmodel.SavedImagesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { EditImageViewModel(editImageRepository =  get()) }
+    viewModel { EditImageViewModel(editImageRepository = get()) }
+    viewModel { SavedImagesViewModel(savedImagesRepository = get()) }
 }
