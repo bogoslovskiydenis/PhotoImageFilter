44030355573c2db7e6e9a381f6540b5f08aa15e3
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-06-17 11:35:44 +0300

    activity SaveImageActivity

--- app/src/main/java/com/example/photoimagefilter/activities/SaveImageActivity.kt
+++ app/src/main/java/com/example/photoimagefilter/activities/SaveImageActivity.kt
@@ -2,11 +2,42 @@ package com.example.photoimagefilter.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.photoimagefilter.R
+import android.view.View
+import com.example.photoimagefilter.databinding.ActivitySaveImageBinding
+import com.example.photoimagefilter.utilities.displayToast
+import com.example.photoimagefilter.viewmodel.SavedImagesViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SaveImageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySaveImageBinding
+    private val viewModel: SavedImagesViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_save_image)
+        binding = ActivitySaveImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupObserver()
+        setListeners()
+        viewModel.loadSavedImages()
+    }
+
+    private fun setupObserver() {
+        viewModel.savedImagesUiState.observe(this, {
+            val savedImagesDataState = it ?: return@observe
+            binding.savedImagesProgressBar.visibility
+            if (savedImagesDataState.isLoading) View.VISIBLE else View.GONE
+            savedImagesDataState.savedImages?.let { savedImage ->
+                displayToast("${savedImage.size} image loaded")
+            } ?: kotlin.run {
+                savedImagesDataState.error?.let { error ->
+                    displayToast(error)
+                }
+            }
+        })
+    }
+
+    private fun setListeners() {
+        binding.imageback.setOnClickListener { onBackPressed() }
     }
 }
